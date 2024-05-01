53cd440fc27cb3fa4fb87b479e6fc29075aec721
Mina Soltani <112659315+MinaSoltani70@users.noreply.github.com>
2024-05-01 10:01:12 +0300

    Footer inserted in all pages

--- js/index.js
+++ js/index.js
@@ -224,3 +224,12 @@ function displayRecipes(recipes) {
 .catch(error => {
     console.error('Error:', error); // Log any errors to the console
 });
+
+// function to scroll to the bottom of the page on click of the CONTACT US button
+
+
+function scrollToFooter() {
+    var footer = document.getElementById('footer');
+    footer.scrollIntoView({ behavior: 'smooth' });
+  }
+  
