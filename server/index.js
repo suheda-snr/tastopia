0119c48cadd0d0e6ea82ffcfcfd508343d7af018
suheda-snr <t3sesu00@students.oamk.fi>
2025-07-21 14:31:07 +0300

    enable cors

--- server/index.js
+++ server/index.js
@@ -26,63 +26,58 @@ const pgSession = require('connect-pg-simple')(session);
 
 // Connect to PostgreSQL database
 (async () => {
-    try {
-      await sequelize.authenticate();
-      console.log('Database connection has been established successfully.');
-  
-      // Sync the Session model with the database to create the 'session' table
-      await Session.sync();
-      console.log('Session table synchronized successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-  })();
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+
+    // Sync the Session model with the database to create the 'session' table
+    await Session.sync();
+    console.log('Session table synchronized successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
 // Instead of using hardcoded value read port from .env file
 const port = process.env.PORT
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:3001', 'https://tastopia.azurewebsites.net'],
+  credentials: true
+}));
 // Specify your frontend origin
 //const frontendOrigin = 'http://127.0.0.1:5500';
-
-// CORS configuration to accept requests from the frontend origin and allow credentials
-/*const corsOptions = {
-  origin: frontendOrigin,
-  credentials: true, // This is important for cookies, authorization headers with HTTPS
-};
-
-app.use(cors(corsOptions));*/
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..')));
 
 const sessionConfig = {
-    name: 'session',
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      httpOnly: true,
-      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-      maxAge: 1000 * 60 * 60 * 24 * 7
-    },
-    store: new pgSession({
-      conString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, // Replace with your PostgreSQL connection string
-      tableName: 'session',
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // Set this to false for SSL/TLS connection
-      }
-    }),
-    unset: "destroy" // Added this line to ensure proper session deletion
-  };
-  
-  app.use(session(sessionConfig));
-  app.use(flash());
-app.use(express.urlencoded({extended:true}))
+  name: 'session',
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  },
+  store: new pgSession({
+    conString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, // Replace with your PostgreSQL connection string
+    tableName: 'session',
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Set this to false for SSL/TLS connection
+    }
+  }),
+  unset: "destroy" // Added this line to ensure proper session deletion
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/',recipeRouter)
+app.use('/', recipeRouter)
 app.use('/', userRoutes);
 app.use('/', shareRecipeRoutes);
 app.use('/', commentRoutes);
@@ -104,6 +99,6 @@ app.get('/api/check-login', (req, res) => {
   }
 });
 
-app.listen(port,() => {
-    console.log(`Server is listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
 })
